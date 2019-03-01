ba0eb7a2532e4bb4c1f29c00bb4c2773bf22ada3
Yudistyra Ochtavian Putra <Yudistyra_OP355@mitrais.com>
2019-03-01 16:30:06 +0700

    first application commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { AppComponent } from './app.component';
+
+/** config angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { LoginComponent } from './login/login.component';
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    /** import ng-zorro-antd root module，you should import NgZorroAntdModule and avoid importing sub modules directly **/
+    NgZorroAntdModule
+  ],
+  bootstrap: [ AppComponent ],
+  /** config ng-zorro-antd i18n (language && date) **/
+  providers   : [
+    { provide: NZ_I18N, useValue: en_US }
+  ]
+})
+export class AppModule { }
